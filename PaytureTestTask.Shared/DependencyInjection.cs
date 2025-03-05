19c9f90964a54e79b6e7c90a0f880e42749cf891
Smakle <SmakleLover@yandex.ru>
2025-03-05 20:22:16 +0300

    Add dependency injections

---
+++ PaytureTestTask.Shared/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PaytureTestTask.Application.Interfaces;
+using PaytureTestTask.Shared.PaytureProvider;
+
+namespace PaytureTestTask.Shared
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddShared(this IServiceCollection services)
+        {
+            services.AddScoped<IPaytureApi, PaytureApi>();
+
+            return services;
+        }
+    }
+}
